829aba62ce5264d64d3c9fe086a9560cee960ac2
kairamkonda vijayasree <75258563+vijayashree-kairamkonda@users.noreply.github.com>
2023-03-05 17:36:08 +0530

    tutorial on Graphql

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes,Route} from "react-router-dom";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Home from "./pages/home";
+import Search from "./pages/search";
+
 
 function App() {
+
+  const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    uri: "https://countries.trevorblades.com",
+  });
   return (
+    <ApolloProvider client={client}>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route path="/" exact element={<Home/>} />
+          <Route path="/search" exact element={<Search/>} />
+      </Routes>
     </div>
+  </ApolloProvider>
   );
 }
 
